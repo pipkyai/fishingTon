7df574632d36d14167148c9096c5a93b9f4027b2
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-04-28 03:37:56 +0300

    first

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ let tg = window.Telegram.WebApp;
 
 
 tg.expand();
+
+let profName = document.createElement('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+   ${tg.initDataUnsafe.user.last_name}
+   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+
+
 function callMe(){
     alert("Hello World!");
 }
