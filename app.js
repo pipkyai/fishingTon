52a33910eba2cd93eef536608f43046c942d77f5
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-04-28 03:44:42 +0300

    first

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ let tg = window.Telegram.WebApp;
 
 
 tg.expand();
-
-let profName = document.createElement('p'); //создаем параграф
-profName.innerText = `${tg.initDataUnsafe.user.first_name}
-   ${tg.initDataUnsafe.user.last_name}
-   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+tg.MainButton.text = "Changed Text"
 
 
 function callMe(){
