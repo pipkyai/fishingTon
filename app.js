ccdcdbc3146b10a655c0a4ea0e7ee5a238b03c41
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-04-28 03:57:37 +0300

    first

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// function setThemeClass() {
-//     document.documentElement.className = Telegram.WebApp.colorScheme;
-// }
-//
-// Telegram.WebApp.onEvent('themeChanged', setThemeClass);
-// setThemeClass();
+
 function callMe(){
     alert("Hello World!");
 }
