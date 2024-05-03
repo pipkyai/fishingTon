da1348770d618a9cf97ee917e745352b74c3b7b9
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-05-04 00:16:54 +0300

    first

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp;
 
 function callMe(){
-    var welcome = 'Hello ' +  tg.initDataUnsafe?.user?.username
+    var welcome = 'Привет ' +  tg.initDataUnsafe?.user?.username
     alert(welcome);
 }
