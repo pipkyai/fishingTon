e9f930de068b1a1dc5ad865667fc32c2dde1db74
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-05-05 00:37:43 +0300

    first

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const tg = window.Telegram.WebApp;
 
 
+window.onload = function() {
+    callMe();
+};
+
 function callMe(){
-    var welcome = 'Привет ' +  tg.initDataUnsafe?.user?.username
-    alert(welcome);
+    document.getElementById("username").textContent='Привет ' +  tg.initDataUnsafe?.user?.username;
 }
 
 function catchFish() {
     alert("Ты поймал рыбу!");
 }
 
-let username = document.getElementById("username");
-username.textContent = tg.initDataUnsafe?.user?.username;
+
+
+
 
 
