460a4e950900cac9306548481d1fcf357f261eda
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-05-30 14:40:21 +0300

    general update

--- app.js
+++ app.js
@@ -26,9 +26,16 @@ function klik() {
         }else{
         document.getElementById("rod").style.opacity = 1;
         var catchedTime = Math.floor(Date.now() / 100);
-        alert(catchedTime - startTime);
+        var duration = catchedTime - startTime;
+        
+        var x = (duration/float[2] - Math.floor(duration/float[2])).toFixed(2);
+        if((x > 0.9)&&(x < 1)){
+            alert("Поймал!");
+            counterIncrement();
+        }else{
+            alert("Не поймал!");
+        }
         load();
-
     }
 }
 
@@ -41,8 +48,14 @@ function load(){
 
 
 function getFloat(){
-    const floats = ["1.gif/143/160", "2.gif/100/110"];
+    const floats = ["1.gif/143/160", "2.gif/100/109"];
     const random = Math.floor(Math.random() * floats.length);
     const floatData = floats[random].split("/");
     return floatData;
+}
+
+function counterIncrement(){
+    var x = Number(document.getElementById("counter").textContent);
+    x++;
+    document.getElementById("counter").textContent = x;
 }
