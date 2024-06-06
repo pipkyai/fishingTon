2d10e71ff42177c8d65af73d3550a3a2b4c843f7
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-06-06 14:56:45 +0300

    apples

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function callMe(){
 
 
 function klik() {
-    alert(JSON.stringify(tg.CloudStorage.getKeys()));
+    alert(JSON.stringify(tg.CloudStorage.getItem("apples")));
     var element = document.getElementById('rod'),
     style = window.getComputedStyle(element),
     opa = style.getPropertyValue('opacity');
